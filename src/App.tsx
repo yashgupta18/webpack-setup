ae9cec2e54d9b7665489bd05ba56dff52865fa63
yashgupta18 <18guptayash@gmail.com>
2022-08-18 12:26:41 +0530

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
+import './App.css'
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
